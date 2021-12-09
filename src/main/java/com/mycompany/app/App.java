f89c338930204d0d65dca87c5ff09c7f0a0a1ab2
Jayanthan J <jayanthan.j@hcl.com>
2021-12-09 21:22:42 +0530

    Week10

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,10 @@
+package com.mycompany.app;
+import java.util.*;
+public class App 
+{
+    public static void main( String[] args )
+    {
+	Scanner ui = new Scanner(System.in);
+        System.out.println( "Hello Dev-Ops" );
+    }
+}
